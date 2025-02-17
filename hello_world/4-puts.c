680c943217f50d87b54bcec9374ff9dc114983ae
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-17 10:34:36 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - print \"Programming is like building a multilingual puzzle
+ * Description: this print \"Programming is like building a multilingual puzzle
+ * Return: 0 if succeded
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
