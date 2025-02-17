b23f961ff4228d6f3e3edefdc6f014336e4beed6
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-17 13:00:34 +0000

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print: with proper grammar, but the outcome is a piece of art,
+ * Description: print: with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if succeded
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n",sizeof(char),sizeof(int),sizeof(long int),sizeof(long long int),sizeof(float));
+	return (0);
+}
