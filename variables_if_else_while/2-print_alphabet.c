1be45665e7e9b976bfa3bfe780dda02207870214
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-17 15:02:43 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print variable n%10 value
+ * Description: print variable n%10 value
+ * Return: 0 if succeded
+ */
+int main(void)
+{
+char lettre
+while (lettre < 27)
+{
+	printf("%c",lettre)
+	lettre++
+	return (0);
+}
+
