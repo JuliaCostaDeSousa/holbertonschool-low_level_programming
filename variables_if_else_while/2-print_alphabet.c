ebf18390e67db7d936c5588935900ebce0e7fdb6
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-18 09:06:46 +0000

    print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - print the alphabet
  * Description: print the alphabet
  * Return: 0 if succeded
+ * @lettre: lettre
+ * @indice: indice
  */
 int main(void)
 {
 	char lettre = 'a';
 	int indice = 1;
+
 	while (indice < 27)
 	{
 		putchar(lettre);
