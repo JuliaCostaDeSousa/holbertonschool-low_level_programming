9af446602b14d8c79ca52d479ea1d672dbf6f1e7
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-18 12:23:47 +0000

    print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10
+ * Description: print all single digit numbers of base 10
+ * Return: 0 if succeded
+ */
+int main(void)
+{
+	int indice = 48;
+
+	while (indice < 58)
+	{
+		putchar(indice);
+		indice++;
+	}
+	putchar('\n');
+return (0);
+}
+
