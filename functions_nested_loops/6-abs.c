11dd89accc9a9bdeb5da6a6f1bee88ef76855d51
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-19 15:03:40 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ *
+ * Return: computes absolute value of an integer
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		*-1
+	}
+
+
+
+}
