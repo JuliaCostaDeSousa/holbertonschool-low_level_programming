30d309a455f4307f471be682f2f864af2f665ed6
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-19 10:44:30 +0000

    print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h" 
+#include "main.h"
 
 /**
  * print_alphabet - write the alphabet
@@ -11,11 +11,11 @@
 void print_alphabet(void)
 {
 	int lettre = 97;
-	while(lettre < 123)
+
+	while (lettre < 123)
 	{
 		_putchar(lettre);
 		lettre++;
 	}
-	_putchar('\n');	
-	return;
+	_putchar('\n');
 }
