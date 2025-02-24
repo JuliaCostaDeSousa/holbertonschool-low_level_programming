ed50b01a89cb974402b1a084a0a27dafa25a9fc4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-24 15:16:05 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+  * _strlen - returns the length of a string
+  * Description: returns the length of a string
+  * @s: string
+  * Return: length of a string
+  */
+int _strlen(char *s)
+{
+        int index = 0;
+
+        while ((*(s + index) != '\0'))
+        {
+                index++;
+        }
+        return (index);
+}
+
+/**
+ * _puts - prints a string followed by newline to stdout
+ * Description: prints a string followed by newline to stdout
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int len, increment;
+	len = _strlen(str);
+	increment = 0;
+	
+	while (increment <= len)
+	{
+		_putchar(*(str + increment));
+		increment++;
+	}
+	_putchar('\n');
+}
