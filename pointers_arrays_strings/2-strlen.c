f28f1807f3f9efb68910fe24bbd11919d8624013
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-24 14:28:08 +0000

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
- /**
+/**
   * _strlen - returns the length of a string
   * Description: returns the length of a string
   * @s: string
   * Return: length of a string
   */
-
 int _strlen(char *s)
 {
 	int index = 0;
