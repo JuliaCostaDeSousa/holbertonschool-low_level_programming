5ee1bdc109113a44ebed3850e615003a5230a942
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-27 08:29:41 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to character
+ * Description: converts a string to character
+ * @s: string
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+
+}
