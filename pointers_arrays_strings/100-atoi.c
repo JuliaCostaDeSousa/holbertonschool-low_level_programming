e299dbe0471022db0bd72b36ea3fab45cf5143ff
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-27 09:23:02 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,14 +19,42 @@ int _atoi(char *s)
 	int index = 0;
 	int signe_moins = 0;
 	int signe_plus = 0;
+	int num = 0;
 
 	if (len =! 0)
 	{
 		for (index = 0; index <= len, index++)
 		{
+			if (s[index] < 48 && s[index] > 57)
+			{
+				if (s[index] == '+')
+				{
+					signe_plus ++;
 
+				}
+				else if (s[index] == '-')
+				{
+					signe_moins++;
+				}
+			}
+			else
+			{
+				char_index_start = index;
+				char_index_end = index;
+				while (s[index] >= 48 && s[index] <= 57)
+				{
+					num = num * 10 + (str[index] - 48);
+				}
 
+				if (signe_plus >= signe_moins)
+				{
+					return(num));	
+				}
+				else
+				{
+					return(0-num);
+				}
+			}
 		}
 	}
-
 }
