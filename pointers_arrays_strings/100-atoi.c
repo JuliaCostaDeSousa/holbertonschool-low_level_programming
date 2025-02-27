a4215ef4e9649314529790e6ff54197d62083523
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-02-27 08:47:02 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,5 +9,24 @@
 
 int _atoi(char *s)
 {
+	int len = 0;
+
+	while ((*(s + len) != '\0'))
+	{
+		len++;
+	}
+	
+	int index = 0;
+	int signe_moins = 0;
+	int signe_plus = 0;
+
+	if (len =! 0)
+	{
+		for (index = 0; index <= len, index++)
+		{
+
+
+		}
+	}
 
 }
