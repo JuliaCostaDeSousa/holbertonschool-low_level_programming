670f1d7f876a37cb7b44e957e00124f0eb2b8288
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-01 16:49:11 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * Description: compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return : int, 0 if equal, < 0 if s1 < s2, > 0 if s1 > s2
+ * Return: int, 0 if equal, < 0 if s1 < s2, > 0 if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
