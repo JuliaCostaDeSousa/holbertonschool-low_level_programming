1905509cfa56acec58def251c7446244ff70df29
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-02 12:26:43 +0000

    leet'

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -5,11 +5,25 @@
  *
  * Return: Always 0.
  */
+
+/*
 int main(void)
 {
 	char *str;
 
 	str = "0123456789";
 	puts_half(str);
+	return (0);
+}
+*/
+
+int main(void)
+{
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
+
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
 	return (0);
 }
