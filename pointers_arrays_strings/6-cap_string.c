50c7e8fe9f2250843a0da12b6d2b48edfbd99d4a
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-02 11:46:00 +0000

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,10 @@ char *cap_string(char *str)
 	int index_str = 0;
 	int index_arr;
 
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		*str -= 32;
+	}
 	for (index_str = 0; *(str + index_str) != '\0'; index_str++)
 	{
 		for (index_arr = 0; index_arr <= 13; index_arr++)
