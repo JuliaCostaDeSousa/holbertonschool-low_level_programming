c1bb5f14b6f9dcdd0863b60b1a59309930ee4b92
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-03 13:39:23 +0000

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,15 +5,16 @@
  * Description: fills memory with a constant byte
  * @s: memory area
  * @b: constant byte
- * @n: first n byte 
+ * @n: first n byte
  * Return: pointer to memory area s
  */
 
-char *_memset( char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index = 0;
 	char *ptr = s;
-	while (index <= n)
+
+	while (index < n)
 	{
 		s[index] = b;
 		index++;
