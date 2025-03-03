251b8db18811820349af8541140b2f450ac39c15
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-03 10:49:21 +0100

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * Description: fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: first n byte 
+ * 
+ */
