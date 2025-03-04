69479e10de8cb91dba8e38307757114a08f291d9
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-04 10:42:09 +0000

    print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,28 +8,19 @@
 
 void _print_rev_recursion(char *s)
 {
-	char *ptr_start = &s[0];
-	int len_s = 0;
-
-	for (; *(s + len_s) != '\0'; len_s++)
-	{
-	}
-
-	char *ptr_end = &s[(len_s - 1)];
-
-	printf("ptr_start = %c", *ptr_start);	
-	printf("ptr_end = %c", *ptr_end);
+	char *ptr_start = s;
 	
-	if (ptr_end != ptr_start)
+	if (*s != '\0')
 	{
-		_putchar(*ptr_end);
-		s = ptr_end;
-		s--;
-		printf("s = %s", s);
+		s++;
 		_print_rev_recursion(s);
 	}
 	else
 	{
-		_putchar(*ptr_start);
+		s--;
+		_putchar(*s);
+		s = ptr_start;
+		s++;
+		_print_rev_recursion(s);
 	}
 }
