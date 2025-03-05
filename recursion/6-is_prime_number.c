0b9649cc1cd8a1e1ada25af84a15d900694628b7
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-05 21:52:00 +0000

    is prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+int check_multiple(int n, int a)
+{
+	if (a < n)
+	{
+		if ((n % a) == 0)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (check_multiple(n, a + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
+
 /**
  * is_prime_number - check if number is prime number
  *
@@ -9,5 +29,23 @@
 
 int is_prime_number(int n)
 {
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		int mult;
 
+		mult = check_multiple(n, 2);
+		
+		if (mult < 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
 }
