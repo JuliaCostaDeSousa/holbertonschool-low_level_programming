6c7db7721be7ce826d5c261f8764a25371a3d97a
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-06 08:19:45 +0000

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,17 +11,14 @@ int main(__attribute__((unused)) int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error");
-		return (1);
+		printf("Error\n");
 	}
 	else
 	{
-		int nb1 = *argv[1];
-		int nb2 = *argv[2];
-		
-		printf("nb1 = %d\n", nb1);
-		printf("nb2 = %d\n", nb2);
-		return (nb1 * nb2);
+		int nb1 = atoi(argv[1]);
+		int nb2 = atoi(argv[2]);
+
+		printf("%d\n", nb1 * nb2);
 	}
 	return (0);
 }
