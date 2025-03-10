b8ffec635e6d8a54be47a8aa704f684de553954b
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-10 11:26:35 +0100

    str_concat

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-char *s;
+char *s = NULL;
 unsigned int index = 0;
 
 	if (size == 0)
--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,22 +1,32 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate string in new allocated space
- * 
- * @str: string to duplicate
- * Returns: a pointer to newly allocated space in memory for duplicated string
- */
+	* _strdup - duplicate string in new allocated space
+	* Description: duplicate string in new allocated space
+	* @str: string to duplicate
+	* Return: a pointer to newly allocated space in memory for duplicated string
+	*/
 
 char *_strdup(char *str)
 {
-    char *str_copy;
-    size_str = malloc(str);
-        if (size_str == NULL)
-        {
-            return (NULL);
-        }
+char *str_copy = NULL;
+unsigned int index = 0;
 
-
-
-
+	if (strlen(str) == 0)
+	{
+		printf("str = 0\n");
+		return (NULL);
+	}
+str_copy = malloc(strlen(str) + 1);
+	if (str_copy == NULL)
+	{
+		printf("fail to allocate memory\n");
+		return (NULL);
+	}
+	while (index <= strlen(str) + 1)
+	{
+		*(str_copy + index) = *(str + index);
+		index++;
+	}
+return (str_copy);
 }
