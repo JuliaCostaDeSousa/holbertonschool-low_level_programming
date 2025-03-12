cbbcd2a76df66a4af64f910d8f4f26b6e21b3fef
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-12 09:18:02 +0100

    strnconcat _strlen

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,20 @@
 #include "main.h"
-
+/**
+  * _strlen - returns the length of a string
+  * Description: returns the length of a string
+  * @s: string
+  * Return: length of a string
+  */
+ unsigned int _strlen(char *s)
+ {
+	 unsigned int index = 0;
+ 
+	 while ((*(s + index) != '\0'))
+	 {
+		 index++;
+	 }
+	 return (index);
+ }
 /**
  * string_nconcat - concatenates two string
  *
@@ -14,50 +29,50 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3 = NULL;
 	unsigned int index = 0;
 
-		if ((s1 == NULL || strlen(s1) == 0) && (s2 == NULL || strlen(s2) == 0))
+		if ((s1 == NULL || _strlen(s1) == 0) && (s2 == NULL || _strlen(s2) == 0))
 		return (NULL);
-		if ((s1 == NULL || strlen(s1) == 0) && strlen(s2) != 0 && n != 0)
+		if ((s1 == NULL || _strlen(s1) == 0) && _strlen(s2) != 0 && n != 0)
 		{
-			if (n < strlen(s2))
+			if (n < _strlen(s2))
 			{
 				s3 = malloc(n + 1);
 			}
 			else
 			{
-				n = strlen(s2) + 1;
-				s3 = malloc(strlen(s2) + 1);
+				n = _strlen(s2) + 1;
+				s3 = malloc(_strlen(s2) + 1);
 			}
 			if (s3 == NULL)
 			return (NULL);
 			for (; index < n - 1; index++)
 			*(s3 + index) = *(s2 + index);
 		}
-		else if ((s2 == NULL || strlen(s2) == 0) && strlen(s1) != 0)
+		else if ((s2 == NULL || _strlen(s2) == 0) && _strlen(s1) != 0)
 		{
-			s3 = malloc(strlen(s1) + 1);
+			s3 = malloc(_strlen(s1) + 1);
 			if (s3 == NULL)
 			return (NULL);
-			for (; index < strlen(s1); index++)
+			for (; index < _strlen(s1); index++)
 			*(s3 + index) = *(s1 + index);
 		}
 		else
 		{
-			if (n < strlen(s2))
+			if (n < _strlen(s2))
 			{
-				s3 = malloc(strlen(s1) + n + 1);
+				s3 = malloc(_strlen(s1) + n + 1);
 			}
 			else
 			{
-				n = strlen(s2);
-				s3 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+				n = _strlen(s2);
+				s3 = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 			}
 			if (s3 == NULL)
 			return (NULL);
-			for (; index < strlen(s1); index++)
+			for (; index < _strlen(s1); index++)
 			*(s3 + index) = *(s1 + index);
-			for (; index < (strlen(s1) + n); index++)
+			for (; index < (_strlen(s1) + n); index++)
 			{
-				*(s3 + index) = *(s2 + (index - strlen(s1)));
+				*(s3 + index) = *(s2 + (index - _strlen(s1)));
 			}
 		}
 	index++;
