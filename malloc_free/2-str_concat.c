28261ab65d54fe6593055b4f9fd734c1468148cf
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-12 09:12:57 +0100

    strconcat _strlen

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,20 @@
 #include "main.h"
-
+/**
+  * _strlen - returns the length of a string
+  * Description: returns the length of a string
+  * @s: string
+  * Return: length of a string
+  */
+ unsigned int _strlen(char *s)
+ {
+	 unsigned int index = 0;
+ 
+	 while ((*(s + index) != '\0'))
+	 {
+		 index++;
+	 }
+	 return (index);
+ }
 /**
  * str_concat - concatenates two strings
  *
@@ -13,33 +28,33 @@ char *str_concat(char *s1, char *s2)
 char *s3 = NULL;
 unsigned int index = 0;
 
-	if ((s1 == NULL || strlen(s1) == 0) && (s2 == NULL || strlen(s2) == 0))
+	if ((s1 == NULL || _strlen(s1) == 0) && (s2 == NULL || _strlen(s2) == 0))
 	return (NULL);
-	if ((s1 == NULL || strlen(s1) == 0) && strlen(s2) != 0)
+	if ((s1 == NULL || _strlen(s1) == 0) && _strlen(s2) != 0)
 	{
-		s3 = malloc(strlen(s2) + 1);
+		s3 = malloc(_strlen(s2) + 1);
 			if (s3 == NULL)
 			return (NULL);
-			for (; index < strlen(s2) + 1; index++)
+			for (; index < _strlen(s2) + 1; index++)
 			*(s3 + index) = *(s2 + index);
 	}
-	else if ((s2 == NULL || strlen(s2) == 0) && strlen(s1) != 0)
+	else if ((s2 == NULL || _strlen(s2) == 0) && _strlen(s1) != 0)
 	{
-		s3 = malloc(strlen(s1) + 1);
+		s3 = malloc(_strlen(s1) + 1);
 			if (s3 == NULL)
 			return (NULL);
-			for (; index < strlen(s1) + 1; index++)
+			for (; index < _strlen(s1) + 1; index++)
 			*(s3 + index) = *(s1 + index);
 	}
 	else
 	{
-		s3 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+		s3 = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 			if (s3 == NULL)
 			return (NULL);
-			for (; index < strlen(s1); index++)
+			for (; index < _strlen(s1); index++)
 			*(s3 + index) = *(s1 + index);
-			for (; index < (strlen(s1) + strlen(s2) + 1); index++)
-			*(s3 + index) = *(s2 + (index - strlen(s1)));
+			for (; index < (_strlen(s1) + _strlen(s2) + 1); index++)
+			*(s3 + index) = *(s2 + (index - _strlen(s1)));
 	}
 return (s3);
 }
