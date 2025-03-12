6a1b0bdb47af745439fa9c583d9167f5fb3d13a2
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-12 11:03:38 +0100

    string nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,77 +5,58 @@
   * @s: string
   * Return: length of a string
   */
- unsigned int _strlen(char *s)
- {
-	 unsigned int index = 0;
- 
-	 while ((*(s + index) != '\0'))
-	 {
-		 index++;
-	 }
-	 return (index);
- }
+unsigned int _strlen(char *s)
+{
+	unsigned int index = 0;
+
+	while ((*(s + index) != '\0'))
+	{
+		index++;
+	}
+	return (index);
+}
 /**
- * string_nconcat - concatenates two string
+ * string_nconcat - concatenates two strings
  *
- * @s1: string to be appended
- * @s2: string to append to s1
- * @n: number of character of s2 to append to s1
- * Return: pointer that contains the concatenation*
+ * @s1: string 1 to be appended
+ * @s2: string to append
+ * Return: pointer to new allocated space with s2 appended to s1
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3 = NULL;
-	unsigned int index = 0;
+char *s3 = NULL;
+unsigned int index = 0;
+unsigned int len_s1;
+unsigned int len_s2;
 
-		if ((s1 == NULL || _strlen(s1) == 0) && (s2 == NULL || _strlen(s2) == 0))
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+len_s1 = _strlen(s1);
+if (n >= _strlen(s2))
+len_s2 = _strlen(s2);
+else
+len_s2 = n;
+
+s3 = malloc(len_s1 + len_s2 + 1);
+	if (s3 == NULL)
+	{
 		return (NULL);
-		if ((s1 == NULL || _strlen(s1) == 0) && _strlen(s2) != 0 && n != 0)
-		{
-			if (n < _strlen(s2))
-			{
-				s3 = malloc(n + 1);
-			}
-			else
-			{
-				n = _strlen(s2) + 1;
-				s3 = malloc(_strlen(s2) + 1);
-			}
-			if (s3 == NULL)
-			return (NULL);
-			for (; index < n - 1; index++)
-			*(s3 + index) = *(s2 + index);
-		}
-		else if ((s2 == NULL || _strlen(s2) == 0) && _strlen(s1) != 0)
-		{
-			s3 = malloc(_strlen(s1) + 1);
-			if (s3 == NULL)
-			return (NULL);
-			for (; index < _strlen(s1); index++)
-			*(s3 + index) = *(s1 + index);
-		}
-		else
-		{
-			if (n < _strlen(s2))
-			{
-				s3 = malloc(_strlen(s1) + n + 1);
-			}
-			else
-			{
-				n = _strlen(s2);
-				s3 = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-			}
-			if (s3 == NULL)
-			return (NULL);
-			for (; index < _strlen(s1); index++)
-			*(s3 + index) = *(s1 + index);
-			for (; index < (_strlen(s1) + n); index++)
-			{
-				*(s3 + index) = *(s2 + (index - _strlen(s1)));
-			}
-		}
-	index++;
-	*(s3 + index) = '\0';
-	return (s3);
+	}
+	for (; index < len_s1; index++)
+	{
+		*(s3 + index) = *(s1 + index);
+	}
+	for (; index < (len_s1 + len_s2); index++)
+	{
+		*(s3 + index) = *(s2 + (index - len_s1));
+	}
+*(s3 + index) = '\0';
+return (s3);
 }
