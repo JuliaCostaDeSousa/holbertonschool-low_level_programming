843775b56a5d6cdaccfabefdfc32831dcae9055a
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-12 11:38:13 +0100

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,17 +13,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr = NULL;
 	unsigned int index = 0;
-	
+
 	if (nmemb == 0 || size == 0)
-        {
-		return (NULL);
-        }
+	return (NULL);
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
-		return (NULL);
-	}
+	return (NULL);
+
 	for (; index < (nmemb * size); index++)
 	{
 		{
