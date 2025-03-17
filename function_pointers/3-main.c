b99fa4c249216cecd7788a444151517968b919bf
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-17 14:27:00 +0100

    exo 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	return (NULL);
+
+	int num1 = 0, num2 = 0;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	
+
+
+}
