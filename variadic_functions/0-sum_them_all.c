4936c066e1f660357fa341b097b8054595e4a3aa
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 08:36:29 +0000

    sum_the_all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,5 +8,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int index = 0;
+	int result = 0;
 
+	if (n == 0)
+	return (0);
+
+	va_start(ap, n);
+	while (index < n)
+	{
+		result += va_arg(ap, int);
+		index++;
+	}
+	va_end(ap);
+	return (result);
 }
