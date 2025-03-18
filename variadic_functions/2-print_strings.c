44bdc4dcb6f0a332ff976ccfce424a412edb0c46
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 08:52:56 +0000

    initialisation des fichiers pour print_strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,12 @@
+#include "variadic_function.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between strings
+ * @n: number of strings
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
