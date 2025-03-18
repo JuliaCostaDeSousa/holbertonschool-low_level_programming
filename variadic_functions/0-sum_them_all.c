e5f95e0038be923de5d9d7f51ae3f0a086ec023d
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 08:12:26 +0000

    sum them all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: nb of parameter
+ * Return: sum of all its parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
