3ec329d8c488ac77e34701bf575886893819fbd4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 08:48:49 +0000

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,5 +8,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	
+	unsigned int index = 0;
+	va_list ap;
+
+	va_start(ap, n);
+	while (index < n - 1)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL)
+		printf("%s", separator);
+		index++;
+	}
+	printf("%d\n", va_arg(ap, int));
+	va_end(ap);
 }
