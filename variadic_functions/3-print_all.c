bb3be4db8eda19d8a7f5bfaea32e84cd0c8f30e8
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 16:20:46 +0000

    print all not finished

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,40 @@
 #include "variadic_functions.h"
+/**
+ *
+ *
+ */
 
+int compare_strings(char *s1, char *s2)
+{
+	int count = 0;
+	int len_s1 = 0, len_s2 = 0;
+
+	for 
+
+
+
+
+}
+
+
+
+
+/**
+  * _strlen - returns the length of a string
+  * Description: returns the length of a string
+  * @s: string
+  * Return: length of a string
+  */
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while ((*(s + index) != '\0'))
+	{
+		index++;
+	}
+	return (index);
+}
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
@@ -9,12 +44,15 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	int index_format = 0;
-	int index_arg = 0;
+	char *format_ref;
+	int len_format_ref = 0, len_format_passed = 0, count = 0;
 
-	while (*(format + index) != '\0')
-	{
-		switch (*(format + index)) {
-			case "%c":
-				printf("%c, 
-	}
+	format_ref = "cifs";
+	len_format_ref = _strlen(format_ref);
+
+	while (
+
+	va_start(ap, len();
+
+
 }
