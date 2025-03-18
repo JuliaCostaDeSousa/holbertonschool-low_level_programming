3bcfb5d39ecb54f03f6e9c73ed4a829ee6c9e1cf
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-18 13:54:34 +0000

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,5 +7,14 @@
 
 void print_all(const char * const format, ...)
 {
+	va_list ap;
+	int index_format = 0;
+	int index_arg = 0;
 
+	while (*(format + index) != '\0')
+	{
+		switch (*(format + index)) {
+			case "%c":
+				printf("%c, 
+	}
 }
