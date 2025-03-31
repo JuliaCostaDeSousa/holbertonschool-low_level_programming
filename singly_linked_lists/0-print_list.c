431558e2e93fa0b4b6a1b0e800c7fd06c1ee4ba1
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-03-31 11:02:23 +0200

    write instructions for files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int node_number = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		h = h->next;
+		node_number++;
+	}
+
+	while(h->str != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		node_number++;
+		if (h->next != NULL)
+		h = h->next;
+		else
+		break;
+	}
+
+	return (node_number);
+}
