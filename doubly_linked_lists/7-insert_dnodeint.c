d18c44b856293e996e4e51c9ff0f139e4f5be3e6
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-01 21:37:18 +0200

    test adding getdnodeint function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: list
+ * @index: nth node
+ * Return: the nth node or NULL if the node does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int node_nb = 0;
+
+	if (head == NULL)
+	return (NULL);
+
+	while (head != NULL && node_nb < index)
+	{
+		head = head->next;
+		node_nb++;
+	}
+	if (head == NULL)
+	return (NULL);
+
+	return (head);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: list
  * @idx: index
@@ -10,7 +35,6 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int node_nb = 0;
 	dlistint_t *temp_list, *next_node, *current_node;
 
 	if (*h == NULL)
@@ -18,16 +42,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	return (add_dnodeint(h, n));
 
-	current_node = *h;
-	while (current_node != NULL && node_nb < idx - 1)
-	{
-		current_node = current_node->next;
-		node_nb++;
-	}
+	current_node = get_dnodeint_at_index(*h, idx);
 	if (current_node == NULL)
 	return (NULL);
 
-	if (idx == node_nb + 2)
+	if (current_node->next == NULL)
 	return (add_dnodeint_end(h, n));
 
 	next_node = current_node->next;
