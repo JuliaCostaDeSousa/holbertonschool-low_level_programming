d8b0e22ab13531ba27703336c0a9dbbdcc7a0e0b
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-01 16:37:18 +0200

    task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: list
+ * @idx: index
+ * @n: data node value
+ * Return: address of the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int node_nb = 0;
+	dlistint_t *temp_list, *next_node, *current_node;
+
+	if (*h == NULL)
+	return (NULL);
+	if (idx == 0)
+	{
+		return(add_dnodeint(h, n));
+	}
+	
+	current_node = *h;
+	while (current_node != NULL && node_nb < idx - 1)
+	{
+		current_node = current_node->next;
+		node_nb++;
+	}
+	if (current_node == NULL)
+	return (NULL);
+	if (node_nb == idx)
+	return(add_dnodeint_end(h, n));
+
+	if ((current_node)->next != NULL)
+	next_node = current_node->next;
+	else
+	next_node = NULL;
+	
+	temp_list = malloc(sizeof(dlistint_t));
+	if (temp_list == NULL)
+	{
+		return (NULL);
+	}
+	temp_list->n = n;
+	temp_list->prev = current_node;
+	temp_list->next = next_node;
+
+	current_node->next = temp_list;
+	next_node->prev = temp_list;
+
+	return (temp_list);
+}
