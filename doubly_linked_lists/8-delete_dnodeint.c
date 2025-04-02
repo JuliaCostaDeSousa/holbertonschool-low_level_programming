23bc7cedeb1a26d559d279ace6bb818922adf081
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-02 10:39:12 +0200

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,43 +10,37 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int node_nb = 0;
-	dlistint_t *temp_node, *next_node, *current_node;
+	dlistint_t *current_node;
 
-	if (!head)
-	{
-		return (-1);
-	}
-	if (*head == NULL || (index == 0 && (*head)->next == NULL))
-	{
-		free(*head);
-		*head = NULL;
-		return (1);
-	}
-	if (index == 0 && (*head)->next != NULL)
-	{
-		temp_node = (*head);
-		(*head) = (*head)->next;
-		free(temp_node);
-		return (1);
-	}
+	if (*head == NULL)
+	return (-1);
+
 	current_node = *head;
-	while (current_node != NULL && node_nb < index - 1)
+	if (index == 0)
+	{
+		(*head) = current_node->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current_node);
+		return (1);
+	}
+
+	while (current_node != NULL && node_nb < index)
 	{
 		current_node = current_node->next;
 		node_nb++;
 	}
 	if (current_node == NULL)
 	return (-1);
-	if (current_node->next->next == NULL)
-	{
-		current_node->next = NULL;
-		temp_node = current_node->next;
-		free(temp_node);
-		return (1);
-	}
-	next_node = current_node->next->next;
-	temp_node = current_node->next;
-	current_node->next = next_node;
-	free(temp_node);
+
+	if (current_node->prev != NULL)
+	current_node->prev->next = current_node->next;
+	if (current_node->next != NULL)
+	current_node->next->prev = current_node->prev;
+
+	free(current_node);
+
 	return (1);
 }
