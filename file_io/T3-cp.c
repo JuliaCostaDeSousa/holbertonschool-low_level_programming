81622ef16624a9a1d9f9e7819164bced66c007f1
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-04 10:41:09 +0200

    Jean Némar

--- file_io/T3-cp.c
+++ file_io/T3-cp.c
@@ -6,6 +6,7 @@
  * @filename: filename
  * @exit_code: exit code
  */
+
 void print_RW_error(char *msg, char *filename, int exit_code)
 {
 	dprintf(STDERR_FILENO, "%s%s\n", msg, filename);
@@ -19,7 +20,7 @@ void print_RW_error(char *msg, char *filename, int exit_code)
  */
 void print_close_error(int fd, int exit_code)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
 	exit(exit_code);
 }
 
@@ -29,11 +30,12 @@ void print_close_error(int fd, int exit_code)
  * @argv: array with values of arg
  * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
 	char *file_from, *file_to;
 	char buffer[1024];
-	ssize_t fd1 = 0, fd2 = 0, bytesRead = 0, bytesWrite = 0;
+	ssize_t fd1 = 0, fd2 = 0, bytesRead = 0;
 
 	if (argc != 3)
 	{
@@ -46,26 +48,24 @@ int main(int argc, char *argv[])
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-		print_RW_error("Error: Can't read from file ", file_from, 98);
-
+	print_RW_error("Error: Can't read from file ", file_from, 98);
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-		print_RW_error("Error: Can't write to ", file_to, 99);
+	print_RW_error("Error: Can't write to ", file_to, 99);
 
-	while ((bytesRead = read(fd1, buffer, sizeof(buffer))) > 0)
+	bytesRead = 1;
+	while (bytesRead > 0)
 	{
-		bytesWrite = write(fd2, buffer, bytesRead);
-		if (bytesWrite == -1)
-			print_RW_error("Error: Can't write to ", file_to, 99);
+		bytesRead = read(fd1, buffer, sizeof(buffer));
+		if (bytesRead == -1)
+		print_RW_error("Error: Can't read from file ", file_from, 98);
+		if (bytesRead != 0)
+		write(fd2, buffer, bytesRead);
 	}
 
-	if (bytesRead == -1)
-		print_RW_error("Error: Can't read from file ", file_from, 98);
-
 	if (close(fd1) == -1)
-		print_close_error(fd1, 100);
+	print_close_error(fd1, 100);
 	if (close(fd2) == -1)
-		print_close_error(fd2, 100);
-
+	print_close_error(fd2, 100);
 	return (0);
 }
