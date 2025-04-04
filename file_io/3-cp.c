3ba01efcdcd2a889da1b8fbf45ff51250be1dedb
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-04 10:59:51 +0200

    Jean Némar

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -43,17 +43,14 @@ int main(int argc, char *argv[])
 
 	file_from = argv[1];
 	file_to = argv[2];
-
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
 	print_RW_error("Error: Can't read from file ", file_from, 98);
-
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	print_RW_error("Error: Can't write to ", file_to, 99);
 
-	bytesRead = 1;
-	while ((bytesRead > 0))
+	while ((bytesRead >= 0))
 	{
 		bytesRead = read(fd1, buffer, 1024);
 		if (bytesRead == -1)
@@ -67,7 +64,6 @@ int main(int argc, char *argv[])
 
 	if (bytesRead == -1)
 	print_RW_error("Error: Can't read from file ", file_from, 98);
-
 	if (close(fd1) == -1)
 	print_close_error(fd1, 100);
 	if (close(fd2) == -1)
