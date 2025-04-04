0b0cf8b078ad05ff797699e018aadc3725e15fda
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-04 10:04:26 +0200

    oihrepf

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_error - print error message to the corresponding error
+ * print_RW_error - print error message to the corresponding error
  * @msg: message to print
  * @filename: filename
  * @exit_code: exit code
@@ -14,7 +14,7 @@ void print_RW_error(char *msg, char *filename, int exit_code)
 }
 
 /**
- * print_error - print error message to the corresponding error
+ * print_close_error - print error message to the corresponding error
  * @fd: file descriptor
  * @exit_code: exit code
  */
@@ -38,29 +38,22 @@ int main(int argc, char *argv[])
 	ssize_t fd1 = 0, fd2 = 0, bytesRead = 0, bytesWrite = 0;
 
 	if (argc != 3)
-	print_RW_error("Usage: cp file_from file_to\n", "", 97);
-
+	print_RW_error("Usage: cp file_from file_to\n", "\0", 97);
 	file_from = argv[1];
 	file_to = argv[2];
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
 	print_RW_error("Error: Can't read from file ", file_from, 98);
-
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		close(fd2);
-		print_RW_error("Error: Can't write to ", file_to, 99);
-	}
+	print_RW_error("Error: Can't write to ", file_to, 99);
 
-	while ((bytesRead = read(fd1, buffer, 1024)) > 0)
+	while ((bytesRead = read(fd1, buffer, sizeof(buffer))) > 0)
 	{
 		bytesWrite = write(fd2, buffer, bytesRead);
 		if (bytesWrite == -1)
 		{
-			close(fd1);
-			close(fd2);
 			print_RW_error("Error: Can't write to ", file_to, 99);
 		}
 	}
@@ -76,6 +69,5 @@ int main(int argc, char *argv[])
 	print_close_error(fd1, 100);
 	if (close(fd2) == -1)
 	print_close_error(fd2, 100);
-
 	return (0);
 }
