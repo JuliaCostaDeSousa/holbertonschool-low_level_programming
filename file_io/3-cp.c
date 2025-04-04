63c8eaad92ac6890ccc0600eebd44831453fb1e3
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-04 10:52:21 +0200

    Jean Némar

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 	if (fd2 == -1)
 	print_RW_error("Error: Can't write to ", file_to, 99);
 
-	while ((bytesRead = read(fd1, buffer, sizeof(buffer))) > 0)
+	while ((bytesRead = read(fd1, buffer, 1024)) > 0)
 	{
 		if (bytesRead == -1)
 		print_RW_error("Error: Can't read from file ", file_from, 98);
