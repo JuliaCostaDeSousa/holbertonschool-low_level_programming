6fe4ea3eafb0b902b8a98601d09497faa5883a67
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-04 09:34:08 +0200

    task 3 always

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,13 +3,24 @@
 /**
  * print_error - print error message to the corresponding error
  * @msg: message to print
- * @filename: file descriptor
+ * @filename: filename
  * @exit_code: exit code
  */
 
-void print_error(char *msg, char *filename, int exit_code)
+void print_RW_error(char *msg, char *filename, int exit_code)
 {
 	dprintf(STDERR_FILENO, "%s%s\n", msg, filename);
+	exit(exit_code);
+}
+
+/**
+ * print_error - print error message to the corresponding error
+ * @fd: file descriptor
+ * @exit_code: exit code
+ */
+void print_close_error(int fd, int exit_code)
+{
+	dprintf(STDERR_FILENO, "Can't close fd %d", fd);
 	exit(exit_code);
 }
 
@@ -27,57 +38,46 @@ int main(int argc, char *argv[])
 	ssize_t fd1 = 0, fd2 = 0, bytesRead = 0, bytesWrite = 0;
 
 	if (argc != 3)
-	print_error("Usage: cp file_from file_to\n", "", 97);
+	print_RW_error("Usage: cp file_from file_to\n", "", 97);
 
 	file_from = argv[1];
 	file_to = argv[2];
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-	print_error("Error: Can't read from file ", file_from, 98);
+	print_RW_error("Error: Can't read from file ", file_from, 98);
 
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		close(fd1);
-		print_error("Error: Can't write to ", file_to, 99);
+		print_RW_error("Error: Can't write to ", file_to, 99);
 	}
 
 	bytesRead = read(fd1, buffer, 1024);
+	
+	if (bytesRead < 0)
+	{
+		close(fd1);
+		close(fd2);
+		print_RW_error("Error: Can't read from file ", file_from, 98);
+	}
+
 	while (bytesRead > 0)
-	{	
+	{
 		bytesWrite = write(fd2, buffer, bytesRead);
 		if (bytesWrite == -1)
 		{
 			close(fd1);
 			close(fd2);
-			print_error("Error: Can't write to ", file_to, 99);
+			print_RW_error("Error: Can't write to ", file_to, 99);
 		}
-		bytesRead = read(fd1, buffer, 1024);
-		while (bytesWrite < bytesRead)
-		{
-			bytesWrite += write(fd2, buffer + bytesWrite, bytesRead - bytesWrite);
-			if (bytesWrite == -1)
-			{
-				close(fd1);
-				close(fd2);
-				print_error("Error: Can't write to ", file_to, 99);
-			}
-		}
-		bytesRead = read(fd1, buffer, 1024);
-	}
-	
-	if (bytesRead == -1)
-	{
-		close(fd1);
-		close(fd2);
-		print_error("Error: Can't read from file ", file_from, 98);
 	}
 
 	if (close(fd1) == -1)
-	print_error("Can't close fd ", file_from, 100);
+	print_close_error(fd1, 100);
 	if (close(fd2) == -1)
-	print_error("Can't close fd ", file_to, 100);
+	print_close_error(fd2, 100);
 
 	return (0);
 }
